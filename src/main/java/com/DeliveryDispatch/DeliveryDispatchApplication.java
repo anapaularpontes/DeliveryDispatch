a089170cf112163900d01754dab09c836533dcd5
Ana Paula Ruiz Pontes <ruizpontesa@student.douglascollege.ca>
2020-05-16 10:46:23 -0700

    First commit

---
+++ src/main/java/com/DeliveryDispatch/DeliveryDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.DeliveryDispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryDispatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DeliveryDispatch/DeliveryDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DeliveryDispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
