6a63b9adb89b6849185c3f42040944c9c5e29df3
Ana Paula Ruiz Pontes <ruizpontesa@student.douglascollege.ca>
2020-07-09 21:19:32 -0700

    Fix error

--- src/test/java/com/DeliveryDispatch/DeliveryDispatchApplicationTests.java
+++ src/test/java/com/DeliveryDispatch/DeliveryDispatchApplicationTests.java
@@ -1,13 +1,8 @@
 package com.DeliveryDispatch;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DeliveryDispatchApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
